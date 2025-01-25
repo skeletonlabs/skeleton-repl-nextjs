9432e9217f7650577b9e481508dae907cab57a9a
longph <longphamduy2002@gmail.com>
2025-01-25 20:10:08 +0200

    Add Prettier, format and few changes to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,32 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin']
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin']
 });
 
 export const metadata: Metadata = {
-  title: "Skeleton Playground - Next.js",
-  description: "Skeleton Playground for Next.js",
+	title: 'Skeleton Playground - Next.js',
+	description: 'Skeleton Playground for Next.js'
 };
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        data-theme="cerberus"
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`} data-theme="cerberus">
+				{children}
+			</body>
+		</html>
+	);
 }
