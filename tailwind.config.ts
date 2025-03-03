92bfa7d79d864733df7aba042eed707a491fdeeb
endigo9740 <gundamx9740@gmail.com>
2025-03-03 14:18:56 -0600

    Prep for migration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,16 +3,8 @@ import type { Config } from 'tailwindcss';
 // https://github.com/tailwindlabs/tailwindcss-forms
 import forms from '@tailwindcss/forms';
 
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-
 export default {
-	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
-		contentPath(import.meta.url, 'react')
-	],
+	content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
 		extend: {
 			colors: {
@@ -21,11 +13,5 @@ export default {
 			}
 		}
 	},
-	plugins: [
-		forms,
-		skeleton({
-			// NOTE: each theme included will increase the size of your CSS bundle
-			themes: [themes.cerberus, themes.rose]
-		})
-	]
+	plugins: [forms]
 } satisfies Config;
